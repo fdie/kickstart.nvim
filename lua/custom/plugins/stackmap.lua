f479cb3af8ee390ee890cd3d6cbbf12da4667b0b
fdie <5943122+fdie@users.noreply.github.com>
2024-03-03 22:57:45 +0100

    dedicated plugin file

--- lua/custom/plugins/stackmap.lua
+++ lua/custom/plugins/stackmap.lua
@@ -3,7 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 
--- vim.cmd(":autocmd BufRead,BufNewFile *.erl set softtabstop=4\n")
-vim.keymap.set("n", "<leader>sj", ":Telescope jumplist\n")
-
-return {}
+return {
+  "stackmap.nvim",
+  dir = "/home/fdie/rainbow/nvim/stackmap.nvim",
+}
